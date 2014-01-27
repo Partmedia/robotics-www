b19bb21fa27bf8f8b59829b5650a64c8024f29c6
Kevin Zheng <kevinz5000@gmail.com>
2014-01-26 21:17:38 -0600

    Import work on new Pelican-based site.

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'PSH Robotics Team'
+SITENAME = u'PSH Robotics'
+SITEURL = ''
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Image Gallery', 'http://pshrobotics.org/gallery/'),
+          ('Old Site', 'http://pshrobotics.org/site-old/'),
+          ('FIRST', 'http://www.usfirst.org/'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/groups/pshrobotics1112/'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Set to our custom theme
+THEME = "theme"
